13fa8d08c1a650a09c3ce25d525fd5b71fcc7129
RanchoNRuva <59092240+RanchoNRuva@users.noreply.github.com>
2022-01-12 18:15:42 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,8 +7,8 @@ from kivy.uix.button import Button
 
 class MainApp(App):
     def build(self):
-        self.screen = GridLayout(cols=2)
-        self.screen.add_widget(Label(text='Enter smoke temperature'))
+        self.screen = GridLayout(rows=4)
+        self.screen.add_widget(Label(text='Enter Conventional Plume/Carbon Dioxide Temperature in Fahrenheit'))
         self.temp = TextInput()
         self.screen.add_widget(self.temp)
         self.submit = Button(text="Submit")
@@ -19,16 +19,16 @@ class MainApp(App):
         return self.screen
 
     def confirm(self, instance):
-        print(int(self.temp.text))
+        #print(int(self.temp.text))
         smoketemp = 0
         smoketemp = int(self.temp.text)
         adjustedtemp = smoketemp * 1.28
         if int(adjustedtemp) <= 355:
-            self.output.text = "1 chamber needs to be deployed."
+            self.output.text = "1 pod needs to be deployed."
         elif int(adjustedtemp) > 355 and int(adjustedtemp) <= 375:
-            self.output.text = "2 chambers need to be deployed."
+            self.output.text = "2 pods need to be deployed."
         elif int(adjustedtemp) > 375:
-            self.output.text = "3 chambers needs to be deployed."
+            self.output.text = "3 pods needs to be deployed."
     
     
 
